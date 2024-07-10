52b0982e46eeb8d46c6a5cf18220bffe9cf782f7
Gilles Querret <g.querret@riverside-software.fr>
2024-07-10 12:36:13 +0200

    Keywords in reverse order

--- index.js
+++ index.js
@@ -11,32 +11,43 @@ let result = {};
 let grammarBlocks2 = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []];
 const re = /(?:\w|-|\()+(?=\s|$)/g
 
+let srcKw = [];
+
 lineReader.on('line', line => {
     let results;
     line = line.toLowerCase();
 
     while ((results = re.exec(line)) !== null) {
-        let kw = results[0];
-        if (kw.indexOf('(') !== -1) {
-            let kwParts = kw.split('(');
-            kw = kwParts[0];
-            grammarBlocks2[kw.charCodeAt(0) - 97].push(kw);
-            let kwComplete = kwParts[1];
-            for (const element of kwComplete) {
-                kw += element;
-                grammarBlocks2[kw.charCodeAt(0) - 97].push(kw);
-            }
-        } else {
-            grammarBlocks2[kw.charCodeAt(0) - 97].push(kw);
-        }
+      srcKw.push(results[0]);
     }
 });
 
 lineReader.on('close', () => {
+  for (const kkw of srcKw.sort()) {
+    let kw = kkw;
+    if (kw.indexOf('(') !== -1) {
+        let kwParts = kw.split('(');
+        kw = kwParts[0];
+        let tmp = [];
+        tmp.push(kw);
+        let kwComplete = kwParts[1];
+        for (const element of kwComplete) {
+            kw += element;
+            tmp.push(kw);
+        }
+        for (const e of tmp.reverse()) {
+          grammarBlocks2[e.charCodeAt(0) - 97].push(e);
+        }
+    } else {
+        grammarBlocks2[kw.charCodeAt(0) - 97].push(kw);
+    }
+  }
+  
+  
     for (var zz = 0; zz < 26; zz++) {
         result['keywords-' + String.fromCharCode(97 + zz)] = 
          {
-            match: "(?i)(?<![\\w-])(" + grammarBlocks2[zz].sort().join('|') + ")(?![\\w-])",
+            match: "(?i)(?<![\\w-])(" + grammarBlocks2[zz].join('|') + ")(?![\\w-])",
             name: "keyword.other.abl"
         }
     }
